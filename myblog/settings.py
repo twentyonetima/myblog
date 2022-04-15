e1549f56cd7919444d38cfa08ff25f6ee4666865
Timofey Petrov <twentyonetima@mail.ru>
2022-04-15 18:54:03 +0300

    Add About Me

--- myblog/settings.py
+++ myblog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'events.apps.EventsConfig',
     'blog.apps.BlogConfig',
+    'about'
 ]
 
 MIDDLEWARE = [
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('events.urls')),
+    path('blog', include('blog.urls')),
+    path('about', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
